39a53943a2dac1fba94d4f0f21e4cd58c3fefc9b
Daniel Z <dzimmer@fzi.de>
2021-09-12 00:09:37 +0200

    Some refactoring

--- test_optimizers.jl
+++ test_optimizers.jl
@@ -3,13 +3,11 @@ include("optimizers/optimizer.jl")
 number_generations = 100
 population_size = 50
 sigma = 1.0
-N = 100
+free_parameters = 100
 
 
 function main()
     optimizer_configuration = Dict("sigma" => sigma, "population_size" => population_size)
-    free_parameters = 100
-
     optimizer = inititalize_optimizer(free_parameters, optimizer_configuration)
 
     for generation = 1:number_generations
