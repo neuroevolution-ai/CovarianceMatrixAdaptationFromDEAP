ebf149c884c19e8d99e1dc1085a55688c57e88f9
Daniel Z <dzimmer@fzi.de>
2021-09-12 00:04:21 +0200

    Removed config dictionary

--- optimizers/cma_es_deap2.py
+++ optimizers/cma_es_deap2.py
@@ -9,12 +9,11 @@ class OptimizerCmaEsDeap():
     def __init__(self, individual_size: int, configuration: dict):
 
         self.individual_size = individual_size
-        config = {"type": "CMA-ES-Deap", "population_size" : configuration["population_size"], "sigma" : configuration["sigma"]}
 
         creator.create("FitnessMax", base.Fitness, weights=(1.0,))
         creator.create("Individual", list, typecode='b', fitness=creator.FitnessMax)
 
-        strategy = cma.Strategy(centroid=[0.0] * individual_size, sigma=config["sigma"], lambda_=config["population_size"])
+        strategy = cma.Strategy(centroid=[0.0] * individual_size, sigma=configuration["sigma"], lambda_=configuration["population_size"])
 
         self.toolbox = base.Toolbox()
         self.toolbox.register("generate", strategy.generate, creator.Individual)
