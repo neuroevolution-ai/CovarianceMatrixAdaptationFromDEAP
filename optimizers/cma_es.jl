19938795c63737406f3c87900019f4492ff99772
Daniel Z <dzimmer@fzi.de>
2021-09-13 16:26:44 +0200

    Added struct for optimizer

--- optimizers/cma_es.jl
+++ optimizers/cma_es.jl
@@ -1,25 +1,51 @@
 using LinearAlgebra
+using Parameters
 
 
-function tell(rewards_training, genomes, B, diagD, sigma, ps, old_centroid, update_count, pc, mu, weights, cs, mueff, chiN, dim, cc)
+@with_kw struct OptimizerCmaEs
+    dim::Any
+    chiN::Any
+    mu::Any
+    weights::Any
+    mueff::Any
+    cc::Any
+    cs::Any
+end
+
+function tell(
+    optimizer,
+    rewards_training,
+    genomes,
+    B,
+    diagD,
+    sigma,
+    ps,
+    old_centroid,
+    update_count,
+    pc,
+)
 
     genomes_sorted = genomes[sortperm(rewards_training, rev = true), :]
 
-    centroid = genomes_sorted[1:mu, :]' * weights
+    centroid = genomes_sorted[1:optimizer.mu, :]' * optimizer.weights
 
     c_diff = centroid - old_centroid
 
     # Cumulation : update evolution path
     ps =
-        (1 - cs) .* ps +
-        sqrt(cs * (2 - cs) * mueff) ./ sigma * B * ((1 ./ diagD) .* B' * c_diff)
+        (1 - optimizer.cs) .* ps +
+        sqrt(optimizer.cs * (2 - optimizer.cs) * optimizer.mueff) ./ sigma *
+        B *
+        ((1 ./ diagD) .* B' * c_diff)
 
     hsig = float(
-        norm(ps) / sqrt(1.0 - (1 - cs)^(2 * (update_count + 1))) / chiN <
-        (1.4 + 2 / (dim + 1)),
+        norm(ps) / sqrt(1.0 - (1 - optimizer.cs)^(2 * (update_count + 1))) /
+        optimizer.chiN < (1.4 + 2 / (optimizer.dim + 1)),
     )
 
-    pc = (1 - cc) * pc + hsig * sqrt(cc * (2 - cc) * mueff) / sigma * c_diff
+    pc =
+        (1 - optimizer.cc) * pc +
+        hsig * sqrt(optimizer.cc * (2 - optimizer.cc) * optimizer.mueff) / sigma * c_diff
 
     return centroid, ps, pc
 
